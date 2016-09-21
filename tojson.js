593f2ce4beea6010d54d4a67bc1c669770644c9e
Kenya-West <Kenya-West@outlook.com>
2016-09-21 21:14:27 +0900

    First aplha. Works!

--- tojson.js
+++ tojson.js
@@ -6,15 +6,18 @@ var armies = {
     sneks: []
 };
 
-var text = fs.readFileSync('doges.txt', 'utf8');
-addToArmy(text, armies.doges);
-var text = fs.readFileSync('cates.txt', 'utf8');
-addToArmy(text, armies.cates);
-var text = fs.readFileSync('sneks.txt', 'utf8');
-addToArmy(text, armies.sneks);
+var text = "";
+text = fs.readFileSync('doges.txt', 'utf8');
+armies.doges = addToArmy(text);
+text = fs.readFileSync('cates.txt', 'utf8');
+armies.cates = addToArmy(text);
+text = fs.readFileSync('sneks.txt', 'utf8');
+armies.sneks = addToArmy(text);
 
 fs.writeFileSync('armies.json', JSON.stringify(armies));
 
-function addToArmy(armyFrom, armyTo) { //1st argument - from (string), 2nd - to (array)
-    armyTo = armyFrom.split(' ');
+function addToArmy(armyFrom) { //argunment - whats army text file you should parse
+    var armyTo = armyFrom.split(' ');
+    armyFrom = "";
+    return (armyTo);
 }
